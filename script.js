2bfe83205df89ea1839aeffc03fc5c9ffd6ed042
Faheema_Fasil <faheemafasil12@gmail.com>
2024-09-02 11:50:52 +0530

    edited

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 gsap.timeline({scrollTrigger:{
     trigger:".two",
-    start: "50% 5%",
+    start: "0% 95%",
     end:"50% 50%",
     scrub:true,
     markers:true,
 }})
+tl.to("#fanta",{
+    top:"150%",
+    left:"0%"
+})
